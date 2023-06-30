84325add82bfbf9554e49cb7caf10c86939108f7
Yash Agarwal <ayash0876@gmail.com>
2023-06-30 13:07:18 +0530

    hello

--- static/css/landingpage.js
+++ static/css/landingpage.js
@@ -4,7 +4,7 @@ const home=document.querySelector('#home')
 const about=document.querySelector('#about')
 const see_demo=document.querySelector('#see-demo')
 const contact=document.querySelector('#contact')
-
+const bground=document.querySelector('.container')
     window.addEventListener('scroll',function(){
         if(window.scrollY>=56){
             navEl.classList.add('navbar-scrolled');
@@ -46,4 +46,36 @@ contact.addEventListener('click',()=>{
         left: 0,
         behavior: "smooth",
       });
+})
+
+const sign_btn=document.querySelector('.btn');
+const login_area=document.querySelectorAll('.containing');
+const close_btn=document.querySelectorAll('.close-btn');
+console.log(sign_btn);
+sign_btn.addEventListener('click',function(){
+    login_area[0].style.display="block";
+    
+})  
+close_btn[0].addEventListener('click',function(){
+    login_area[0].style.display="none";
+    login_container.style.display="flex";
+    signin_container.style.display="none";
+    login_area[0].style.transform="rotateY(0deg)";
+})
+close_btn[1].addEventListener('click',function(){
+    login_area[0].style.display="none";
+    login_container.style.display="flex";
+    signin_container.style.display="none";
+    login_area[0].style.transform="rotateY(0deg)";
+})
+
+const new_user=document.querySelector('.new-user');
+const login_container=document.querySelector('.login-container');
+const signin_container=document.querySelector('.signin-container')
+new_user.addEventListener('click',function(){
+    login_area[0].style.transform="rotateY(180deg)";
+    setTimeout(() => {
+        login_container.style.display="none";
+        signin_container.style.display="flex";
+    }, 250);
 })
